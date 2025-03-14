60e1ba745c8cc98c94223955162c96ae21ce47dd
ss <157017006+ce6ac@users.noreply.github.com>
2025-03-14 19:02:54 +0100

    update run.sh (example)

--- client/build/run.sh
+++ client/build/run.sh
@@ -3,19 +3,24 @@
 # example run.sh for cs2hack
 
 # server related params
-# -url      - the url cs2hack server is hosted on
-# -key      - the key set on server to accept data
-# -refresh  - time in ms between updates of player data, which also means time in between requests to server
+# -url    <string>   - the url cs2hack server is hosted on
+# -ep     <string>   - the endpoint to display data on on the webserver
+# -key    <string>   - the key set on server to accept data
+# -refresh <value>   - time in ms between updates of player data, which also means time in between requests to server
 
-# aim/trigger related params
-# -port     - the port qmp can be accessed on (required for aimbot & trigger to work)
-# -fov      - aimbot fov in pixels
-# -smooth   - aimbot smoothness
-# -shots    - maximum of shots aimbot will be enabled for
-# -delay    - time in ms to wait before triggerbot shoots
-# -cooldown - time in ms to wait after triggerbot has shot
-# -w        - ingame resolution width (required for aimbot to work)
-# -h        - ingame resolution height (required for aimbot to work)
+# aim/trigger related params:
+# -port     <value>  - the port qmp can be accessed on (required for aimbot & trigger to work)
+# -fov      <value>  - aimbot fov in pixels
+# -smooth   <value>  - aimbot smoothness
+# -shots    <value>  - maximum of shots aimbot will be enabled for
+# -vischeck          - uses vischeck param if added
+# -delay    <value>  - time in ms to wait before triggerbot shoots
+# -cooldown <value>  - time in ms to wait after triggerbot has shot
+# -team              - includes teammates as targets
+
+# required for aimbot:
+# -w <value>         - ingame resolution width
+# -h <value>         - ingame resolution height
 
 sudo setcap 'CAP_SYS_PTRACE=ep' cs2hack
 ./cs2hack \
@@ -26,6 +31,7 @@ sudo setcap 'CAP_SYS_PTRACE=ep' cs2hack
 -fov 20 \
 -smooth 6 \
 -shots 2 \
+-vischeck \
 -delay 25 \
 -cooldown 80 \
 -w 1440 \
